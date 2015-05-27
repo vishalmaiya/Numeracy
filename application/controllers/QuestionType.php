9d85d43999a7e077d6cab47f01819873bbfbe3b0
Vishal Maniya <vishalmaniya@gmail.com>
2015-05-27 04:37:32 -0400

    questiontype

--- application/controllers/QuestionType.php
+++ application/controllers/QuestionType.php
@@ -13,20 +13,37 @@ class QuestionType extends CI_Controller {
             redirect("admin/login");  //for example
         }
         $this->load->helper(array('form'));
-        
+        $this->load->model('questiontypemodel');
+        if(isset($_POST['qtype']) && $_POST['qtype'] == "deltype")
+       {
+            $tid = $_POST['tid'];
+            $this->questiontypemodel->row_delete($tid);
+       }
     }
     
     function all_type()
     {
-      
        //set validation rules
+       
+       
         $this->load->library('form_validation');
-        $this->form_validation->set_rules('qtype', 'Question Type', 'required');
-        $this->form_validation->set_rules('qparent', 'Question Sub Type', 'trim|required|numeric');
+        if(isset($_POST['taction']) && $_POST['taction'] == "addtype")
+        {
+            $this->form_validation->set_rules('qtype', 'Question Type', 'required');    
+        }
         if ($this->form_validation->run() == FALSE)
         {
-          $data['body'] = 'AllType';
-          $this->load->view('template',$data);
+            $all_parents = $this->questiontypemodel->get_parent_type();
+            foreach($all_parents as $parent)
+            {
+                $all_subtypes = $this->questiontypemodel->get_sub_type($parent->id);
+                $parent->sub_type = $all_subtypes;
+                $types[]=$parent;
+            }
+            $data['alltypes'] = $types;
+            $data['content'] = "AddType";
+            $data['body'] = 'AllType';
+            $this->load->view('template',$data);
         }
         else
         {
@@ -34,11 +51,18 @@ class QuestionType extends CI_Controller {
                 'type' => $this->input->post('qtype'),
                 'parent_id' => $this->input->post('qparent'),
                 );
-            //Transfering data to Model
-            $this->load->model('questiontypemodel');
             $this->questiontypemodel->insert($data);
+            $this->load->model('questiontypemodel');
+            $all_parents = $this->questiontypemodel->get_parent_type();
+            foreach($all_parents as $parent)
+            {   
+                $all_subtypes = $this->questiontypemodel->get_sub_type($parent->id);
+                $parent->sub_type = $all_subtypes;
+                $types[]=$parent;                
+            }
             $data['message'] = 'Record Inserted Successfully';
-            //Loading View
+            $data['alltypes'] = $types;
+            $data['content'] = "AddType";
             $data['body'] = 'AllType';
             $this->load->view('template',$data);
         }  
@@ -51,9 +75,43 @@ class QuestionType extends CI_Controller {
         
     function edit_type()
     {
-        $data['body'] = 'EditType';
-       $this->load->view('template',$data);
-    }
+        if(!isset($_GET['tid']))
+        {
+            redirect("question-type");
+            exit;
+        }
+      $tid = $_GET['tid'];
+        $this->load->library('form_validation');
+        if(isset($_POST['taction']) && $_POST['taction'] == "edittype")
+        {
+            $this->form_validation->set_rules('qtype', 'Question Type', 'required');    
+        }
+        
+        if ($this->form_validation->run() == TRUE)
+        {
+           $data = array(
+                'type' => $this->input->post('qtype')
+                );
+            $this->questiontypemodel->update_type($tid,$data);
+            $data['message'] = 'Record updated Successfully';
+        }
+        
+        $all_parents = $this->questiontypemodel->get_parent_type();
+        foreach($all_parents as $parent)
+        {   
+            $all_subtypes = $this->questiontypemodel->get_sub_type($parent->id);
+            $parent->sub_type = $all_subtypes;
+            $types[]=$parent;                
+        }
+        $singlerecord = $this->questiontypemodel->get_single($tid);
+        $data['single_record'] = $singlerecord;
+        $data['alltypes'] = $types;
+        $data['content'] = "EditType";
+        $data['body'] = 'AllType';
+        $this->load->view('template',$data);
+    } 
+    
+    
     
 }
 ?>
